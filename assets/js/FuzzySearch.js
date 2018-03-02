fa0cc0dca250c9fdad2e09bec6ac72fe2742f6c2
svub <sven.buschbeck@gmail.com>
2018-03-01 21:53:32 -0600

    result snippets w/t md

--- assets/js/FuzzySearch.js
+++ assets/js/FuzzySearch.js
@@ -1,105 +1,108 @@
 class FuzzySearch {
-  constructor(haystack = [], keys = [], options = {}) {
-    if (haystack.length === 0) {
-      throw new Error('We need an array containing the search list');
-    }
-
-    this.haystack = haystack;
-    this.keys = keys;
-    this.options = Object.assign({
-      caseSensitive: false,
-      sort: false,
-    }, options);
-  }
-
-  search(query = '') {
-    if (query === '') {
-      return this.haystack;
-    }
-
-    const results = [];
-
-    for (let i = 0; i < this.haystack.length; i++) {
-      const item = this.haystack[i];
-
-      if (this.keys.length === 0) {
-        const result = FuzzySearch.isMatch(item, query, this.options.caseSensitive);
-
-        if (result.found) {
-          results.push({ item, result });
+    constructor(haystack = [], keys = [], options = {}) {
+        if (haystack.length === 0) {
+            throw new Error('We need an array containing the search list');
         }
-      } else {
-        for (let y = 0; y < this.keys.length; y++) {
-          const propertyValues = Helper.getDescendantProperty(item, this.keys[y]);
 
-          let found = false;
+        this.haystack = haystack;
+        this.keys = keys;
+        this.options = Object.assign({
+            caseSensitive: false,
+            sort: false,
+        }, options);
+    }
 
-          for (let z = 0; z < propertyValues.length; z++) {
-            const result = FuzzySearch.isMatch(propertyValues[z], query, this.options.caseSensitive);
+    search(query = '') {
+        if (query === '') {
+            return this.haystack;
+        }
 
-            if (result.found) {
-              found = true;
+        const results = [];
 
-              results.push({ item, result });
+        for (let i = 0; i < this.haystack.length; i++) {
+            const item = this.haystack[i];
 
-              break;
+            if (this.keys.length === 0) {
+                const result = FuzzySearch.isMatch(item, query, this.options.caseSensitive);
+
+                if (result.found) {
+                    results.push({ item, result });
+                }
+            } else {
+                for (let y = 0; y < this.keys.length; y++) {
+                    const propertyValues = Helper.getDescendantProperty(item, this.keys[y]);
+
+                    let found = false;
+
+                    for (let z = 0; z < propertyValues.length; z++) {
+                        const result = FuzzySearch.isMatch(propertyValues[z], query, this.options.caseSensitive);
+
+                        if (result.found) {
+                            found = true;
+
+                            results.push({ item, result });
+
+                            break;
+                        }
+                    }
+
+                    if (found) {
+                        break;
+                    }
+                }
             }
-          }
-
-          if (found) {
-            break;
-          }
         }
-      }
+
+        if (this.options.sort) {
+            results.sort((a, b) => a.result.score - b.result.score);
+        }
+
+        return results;
     }
 
-    if (this.options.sort) {
-      results.sort((a, b) => a.result.score - b.result.score);
+    // ngram based search
+    static isMatch(item, query, caseSensitive = false) {
+        const GRAM_LENGTH = 3,
+            SNIPPET_LENGTH = 70,
+            orgItem = item;
+        let result = (found, score, location, snippet) => { return { found, score, location, snippet } }
+
+        if (!caseSensitive) {
+            item = item.toLocaleLowerCase();
+            query = query.toLocaleLowerCase();
+        }
+
+        // ngrams
+        let ngrams = []
+        for (var i = 0; i <= query.length - GRAM_LENGTH; i++) {
+            ngrams.push(query.substr(i, GRAM_LENGTH));
+        }
+
+        // search
+        const indexes = [];
+        let index = 0;
+        for (let ngram of ngrams) {
+            index = item.indexOf(ngram, index);
+            if (index === -1) return result(false);
+            indexes.push(index++);
+        }
+
+        // scoring
+        let score = 1;
+        if (item !== query) {
+            score = indexes.reduce((s, i, ii, a) => s + i - (a[ii-1] || a[0]), -indexes.length+2);
+        }
+
+        // snippet
+        index = indexes[0];
+        let snippet = orgItem.substr(Math.max(0, index-SNIPPET_LENGTH/4), SNIPPET_LENGTH);
+        while (snippet.length > SNIPPET_LENGTH-10 && snippet.charAt(snippet.length-1) != ' ') {
+            snippet = snippet.substr(0, snippet.length-1);
+        }
+        while (snippet.length > SNIPPET_LENGTH-20 && snippet.charAt(0) != ' ') {
+            snippet = snippet.substr(1, snippet.length-1);
+        }
+
+        return result(true, score, index, snippet);
     }
-
-    return results;
-  }
-
-  // ngram based search
-  static isMatch(item, query, caseSensitive = false) {
-    const GRAM_LENGTH = 3,
-          SNIPPET_LENGTH = 80,
-          orgItem = item;
-    let result = (found, score, location, snippet) => { return { found, score, location, snippet } }
-
-    if (!caseSensitive) {
-      item = item.toLocaleLowerCase();
-      query = query.toLocaleLowerCase();
-    }
-
-    // ngrams
-    let ngrams = []
-    for (var i = 0; i <= query.length - GRAM_LENGTH; i++) {
-        ngrams.push(query.substr(i, GRAM_LENGTH));
-    }
-
-    // search
-    const indexes = [];
-    let index = 0;
-    for (let ngram of ngrams) {
-      index = item.indexOf(ngram, index);
-      if (index === -1) return result(false);
-      indexes.push(index++);
-    }
-
-    // scoring
-    let score = 1;
-    if (item !== query) {
-        score = indexes.reduce((s, i, ii, a) => s + i - (a[ii-1] || a[0]), -indexes.length+2);
-    }
-
-    // snippet
-    index = indexes[0];
-    let snippet = orgItem.substr(Math.max(0, index-SNIPPET_LENGTH/4), SNIPPET_LENGTH);
-    while (snippet.length > SNIPPET_LENGTH-10 && snippet.charAt(snippet.length-1) != ' ') {
-        snippet = snippet.substr(0, snippet.length-1);
-    }
-
-    return result(true, score, index, snippet);
-  }
 }
